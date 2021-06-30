7c55581f67011e8d8f47b76109ab5a9ba76f205b
Hadjira <bkh.hadjira@gmail.com>
2021-06-30 23:52:39 +0000

    add move commande

--- client.js
+++ client.js
@@ -9,10 +9,42 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on("connect", () => {
+    let myName = 'had'; 
     // code that does something when the connection is first established
-    console.log('you ded cuz you idled');
+    console.log("Successfully connected to game server"); 
+    // send string as a data for server 
+    conn.write(`Name: ${myName}`);
+   
+    setInterval(() => { 
+      conn.write(`Move: up`);
+    }, 500);
+    setInterval(() => { 
+      conn.write(`Move: down`);
+    }, 500);
+    
+    setInterval(() => {
+      conn.write(`Move: left`);
+       
+    }, 800);
+   
+    setInterval(() => {
+      conn.write(`Move: right`);
+      
+    }, 800);
+    
+     
   });
-
+   
+  conn.on('data', (data) => {
+    console.log(data);
+    // conn.write(`Move: up`);
+    // conn.write(`Move: down`);
+   setTimeout(() => {
+   
+     console.log("I move ")
+   }, 1000);
+   console.log('My data : ', data)
+  });
   return conn;
 };
 console.log("Connecting ...");
