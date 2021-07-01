b970f076a1c2e7d058ed09942ce843700c3c002f
NHadjiraIA <bkh.hadjira@gmail.com>
2021-07-01 13:22:33 -0400

    Co-authored-by: LoveJ <lovemorejokonya@users.noreply.github.com>

--- client.js
+++ client.js
@@ -48,18 +48,3 @@ const connect = function () {
 };
 console.log("Connecting ...");
 connect();
-console.log("Deconnecting ...");
-
-// fileA.js
-
-// const myNumber = 42;
-// const myString = "hello";
-// const myFunction = () => {
-//   // myFunction's code
-// };
-
-// module.exports = {
-//   myNumber, // stores 42 as myNumber
-//   myString, // stores "hello" as myString
-//   myFunction, // stores the function as myFunction
-// };
